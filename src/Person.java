82d3ccb58208ba3e750bdd5fd1bf503409e17009
DeathElementNet <yura_99@inbox.ru>
2023-03-05 00:15:47 +0300

    Public Person

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,13 @@
 public class Person {
+    private String name;  //имя
+    private String surname;  //фамилия
+    private int tickets; //билеты
+
+    public Person(String name, String surname, int tickets) { //создание человека с билетами
+        this.name = name;
+        this.surname = surname;
+        this.tickets = tickets;
+    }
+
+
 }
